606abc9f2c369b454e7f2530b09f83a032128363
Robin Millette <robin@millette.info>
2017-01-28 22:53:14 -0500

    Wrap rejected promise with Boom.

--- server/main/index.js
+++ server/main/index.js
@@ -1,34 +1,28 @@
 'use strict'
 
 const utils = require('../../lib/utils')
+const Boom = require('boom')
 
 exports.register = (server, options, next) => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      reply('hi')
-    }
-  })
-
   const fetchWeek = function (request, reply) {
-    reply(utils.week(request.params.week, { onlyBody: true }))
+    reply(utils.week(request.params.week, { onlyBody: true })
+      .catch((e) => Boom.wrap(e, 400, e.message)))
   }
 
   server.route({
     method: 'GET',
+    path: '/',
+    handler: function (request, reply) { reply('hi') }
+  })
+
+  server.route({
+    method: 'GET',
     path: '/week/{week}',
-    config: {
-      pre: [{ method: fetchWeek, assign: 'week' }]
-    },
-    handler: function (request, reply) {
-      reply.view('ack', request.pre.week)
-    }
+    config: { pre: [{ method: fetchWeek, assign: 'week' }] },
+    handler: function (request, reply) { reply.view('ack', request.pre.week) }
   })
 
   next()
 }
 
-exports.register.attributes = {
-  name: 'main'
-}
+exports.register.attributes = { name: 'main' }
