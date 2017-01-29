303ecb639591fe9649b1c618ff2d85b93e3bb055
Robin Millette <robin@millette.info>
2017-01-28 20:21:56 -0500

    More unit tests.

--- test.js
+++ test.js
@@ -15,3 +15,16 @@ test('all', async t => {
   const keys = Object.keys(result[0]).sort()
   t.deepEqual(keys, ['_id', 'description', 'end', 'html', 'location', 'start', 'summary', 'url'])
 })
+
+test('getIds', async t => {
+  const result = await utils.getIds()
+  t.truthy(result.headers)
+  t.truthy(result.body)
+  t.truthy(result.body.rows)
+  t.truthy(result.body.rows.length)
+})
+
+test('getIds, only body', async t => {
+  const result = await utils.getIds({ onlyBody: true })
+  t.truthy(result.rows.length)
+})
