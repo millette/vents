56c195a05aa5f85ae6e94a48ec8e65fd85dd7c28
Robin Millette <robin@millette.info>
2017-01-29 17:01:49 -0500

    Fix html and location in db.

---
+++ fix.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// npm
+const updateNotifier = require('update-notifier')
+
+updateNotifier({ pkg: require('./package.json') }).notify()
+
+// self
+const utils = require('./lib/utils')
+
+const bulk = (data) => utils.bulk(data, { onlyBody: true, auth: true })
+
+const fixDoc = (doc) => {
+  doc.html = utils.makeHtml(doc.description)
+  doc.location = utils.makeLocation(doc.location.source)
+  return doc
+}
+
+// limit: 3, startkey: 1533,
+utils.getIds({ onlyDocs: true, query: { include_docs: true, reduce: false } })
+  .then((x) => x.map(fixDoc))
+  .then(bulk)
+  .then((x) => {
+    console.log(x)
+  })
+  .catch(console.error)
