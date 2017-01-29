0832cb0b644b761371dc35d502afa88109d591db
Robin Millette <robin@millette.info>
2017-01-29 01:58:56 -0500

    Use definition list.

--- templates/helpers/main.js
+++ templates/helpers/main.js
@@ -5,7 +5,12 @@ const helpers = {
   duration: (doc) => {
     const duration = new Date(Date.parse(doc.end) - Date.parse(doc.start))
     const days = duration.getUTCDate() - 1
-    const hours = duration.toISOString().split('T')[1].split(':').slice(0, 2).join('H')
+    const hours = duration.toISOString()
+      .split('T')[1]
+      .split(':')
+      .slice(0, 2)
+      .join('H')
+      .replace(/^0/, '')
     doc.duration = days ? `${days} jours et ${hours}` : hours
     return doc.duration
   }
