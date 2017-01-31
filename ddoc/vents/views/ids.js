f89f95457bd66b6afb46ca5378fd90ea4afb3f4d
Robin Millette <robin@millette.info>
2017-01-31 09:36:10 -0500

    Ids view now only show agendadulibre events.

--- ddoc/vents/views/ids.js
+++ ddoc/vents/views/ids.js
@@ -6,6 +6,7 @@ module.exports = {
   map: function (doc) {
     if (!doc.url) { return }
     var x = doc.url.split('/')
+    if (x[2] !== 'agendadulibre.qc.ca') { return }
     emit(parseInt(x[x.length - 1], 10))
   },
   reduce: '_count'
