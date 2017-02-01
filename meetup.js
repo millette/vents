dc5ae70c568c1ce9822ba4909a55c5d7a8d5b7e0
Robin Millette <robin@millette.info>
2017-01-31 20:51:03 -0500

    Find Québec groups.

--- meetup.js
+++ meetup.js
@@ -3,17 +3,29 @@
 // self
 const meetupApi = require('./lib/meetup-api')
 
+/*
 meetupApi.getGroups('Linux-Montreal')
   .then((x) => {
     console.log('h:', x.headers)
-    // console.log('b:', JSON.stringify(x.body, null, '  '))
+    console.log('b:', JSON.stringify(x.body, null, '  '))
   })
   .catch(console.error)
+*/
 
+/*
 meetupApi.similarGroups('Linux-Montreal')
   .then((x) => {
     console.log('sim h:', x.headers)
     console.log('sim l:', x.body.length)
     // console.log('sim b:', JSON.stringify(x.body, null, '  '))
+  })
+  .catch(console.error)
+*/
+
+meetupApi.findQcGroups(34)
+  .then((x) => {
+    console.log('fg l:', x.headers)
+    console.log('fg l:', x.body.length)
+    console.log('sim b:', JSON.stringify(x.body[0], null, '  '))
   })
   .catch(console.error)
